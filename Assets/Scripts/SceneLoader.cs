0c4f959ceb3c21a204d1d51e4c95ae54c605d7f1
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-05 23:45:36 +0100

    Buttons to change scenes plugged

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+	public void loadNextScene() {
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex + 1);
+	}
+
+	public void loadFirstScene() {
+		SceneManager.LoadScene(0);
+	}
+}

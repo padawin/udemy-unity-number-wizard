bbea28df5af7487ad5632dda89468b86c1159471
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-07 22:16:00 +0100

    Scripts and rules added and plugged

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+	[SerializeField] Rules rules;
+	[SerializeField] SceneLoader sceneLoader;
+	int turn = 0;
+	int? min = null;
+	int? max = null;
+	int? guess = null;
+
+	// Use for initialization
+	void Start() {
+		min = rules.getMin();
+		max = rules.getMax();
+		_guess();
+	}
+
+	void _guess() {
+		guess = (max + min) / 2;
+		turn++;
+		if (turn > rules.getMaxTurns()) {
+			sceneLoader.loadNextScene();
+		}
+	}
+
+	public void clickGuessTooLow() {
+		min = guess;
+		_guess();
+	}
+
+	public void clickGuessTooHigh() {
+		max = guess;
+		_guess();
+	}
+}
---
+++ Assets/Scripts/Rules.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Rules")]
+public class Rules : ScriptableObject {
+	[SerializeField] int min;
+	[SerializeField] int max;
+
+	public int getMin() {
+		return min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public int getMaxTurns() {
+		int turns = Mathf.FloorToInt(Mathf.Log(max, 2));
+		// -1 to not be able to find some values, to make the computer lose
+		return turns;
+	}
+}
